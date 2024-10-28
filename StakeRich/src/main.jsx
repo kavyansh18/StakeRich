8dff51a127c51a9cac59338633f412b303eeba96
kavyansh18 <kavyanshkumar18@gmail.com>
2024-10-28 17:49:58 +0530

    feat:hash-routing

--- StakeRich/src/main.jsx
+++ StakeRich/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css';
 import { AppKitProvider } from '../src/config/Web3ModalProvider'; 
 import { CharacterAnimationsProvider } from '../src/models/Contexts/CharcaterAnimation.jsx'; 
 import CustomCursor from './components/CustomCursor.jsx'; 
+import { HashRouter } from 'react-router-dom'; 
 
 import { Buffer } from 'buffer';
 window.Buffer = Buffer;
@@ -12,13 +13,14 @@ window.Buffer = Buffer;
 import process from 'process';
 window.process = process;
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppKitProvider>
       <CharacterAnimationsProvider>
         <CustomCursor />
-        <App />
+        <HashRouter> 
+          <App />
+        </HashRouter>
       </CharacterAnimationsProvider>
     </AppKitProvider>
   </React.StrictMode>
